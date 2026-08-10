ef97dfd23f162b207f962e1e56a30326785ba0b1
headdrop <ehdgmltn@gmail.com>
2026-08-10 17:14:54 +0900

    Feature: 초대 코드 기반 아지트 랜딩 조회 UseCase 추가

---
+++ src/main/java/com/plip/agit/application/exception/AgitNotFoundException.java
@@ -0,0 +1,12 @@
+package com.plip.agit.application.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class AgitNotFoundException extends RuntimeException {
+
+	public AgitNotFoundException() {
+		super("아지트를 찾을 수 없습니다.");
+	}
+}
