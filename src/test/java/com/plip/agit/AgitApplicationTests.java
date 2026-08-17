2a343860cf3165e05b853eeed1726f5940494d09
headdrop <ehdgmltn@gmail.com>
2026-08-18 00:12:42 +0900

    Fix: CI 테스트 실패 수정

--- src/test/java/com/plip/agit/AgitApplicationTests.java
+++ src/test/java/com/plip/agit/AgitApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class TemplateApplicationTests {
+class AgitApplicationTests {
 
 	@Test
 	void contextLoads() {
