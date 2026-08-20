4468d7f6849724e0328fa1bbb8d90461558c9358
headdrop <ehdgmltn@gmail.com>
2026-08-20 11:37:30 +0900

    Feature: Access JWT 검증 인프라와 Security 설정 추가

---
+++ src/main/java/com/plip/agit/global/config/JwtProperties.java
@@ -0,0 +1,15 @@
+package com.plip.agit.global.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+
+	private String secret;
+}
