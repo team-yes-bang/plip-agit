8b236587a01e9a0038b0fc25913ae31be22ec507
headdrop <ehdgmltn@gmail.com>
2026-08-23 21:55:29 +0900

    Chore: agit JWT 파싱과 JWT_SECRET 의존성 제거
    액터는 Gateway X-User-UUID만 신뢰하고, OpenAPI는 클라 Bearer 스키마만 남긴다.

--- src/main/java/com/plip/agit/application/port/out/AccessTokenPort.java
+++
@@ -1,9 +0,0 @@
-package com.plip.agit.application.port.out;
-
-import java.util.Optional;
-import java.util.UUID;
-
-public interface AccessTokenPort {
-
-	Optional<UUID> parseAccessToken(String token);
-}
